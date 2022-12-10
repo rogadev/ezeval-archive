990b8a9cf2003764e1d80c152ca9fd0af9c834b2
vonroga <ryanroga@gmail.com>
2022-12-10 14:52:00 -0800

    Adds Partytown

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -4,24 +4,39 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/color-mode',
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/partytown',
     'nuxt-icon'
   ],
   runtimeConfig: {
-    // The private keys which are only available server-side
-    apiSecret: '123',
-    // Keys within public are also exposed client-side
+    // apiSecret: '123',
     public: {
       apiBase: '/api'
     }
   },
   colorMode: {
-    preference: 'dark', // default value of $colorMode.preference
+    preference: 'system', // default value of $colorMode.preference
     fallback: 'dark', // fallback value if not system preference found
     hid: 'nuxt-color-mode-script',
     globalName: '__NUXT_COLOR_MODE__',
     componentName: 'ColorScheme',
-    classPrefix: '',
-    classSuffix: '-mode',
+    classSuffix: '',
     storageKey: 'nuxt-color-mode'
-  }
+  },
+  partytown: {
+    forward: ['dataLayer.push'],
+  },
+  app: {
+    head: {
+      script: [
+        // Insert your Google Tag Manager Script here
+        {
+          src: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-W48JKVS');`, async: true, type: 'text/partytown'
+        },
+      ],
+    },
+  },
 });
