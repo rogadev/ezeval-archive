544c3d86b6f2b97c76acc82e25dfaa35f441206d
Roga <ryanroga@gmail.com>
2023-01-05 15:22:04 -0400

    UI Updates

--- sveltekit/src/routes/+layout.svelte
+++ sveltekit/src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
   import "../app.css";
+  import Navbar from '$lib/components/navbar/Navbar.svelte';
 </script>
 
+<Navbar />
 <slot />
---
+++ sveltekit/src/routes/sponsors/+page.svelte
@@ -0,0 +1,12 @@
+<div class="container w-4/5 mx-auto">
+  <article class="prose">
+    <h2>Sponsor Our Project</h2>
+    <p>Sponsoring the EzEval project is a fantastic opportunity for businesses that want to support the growth of a valuable tool in the window cleaning industry and gain exposure for their own brand. By becoming a sponsor, you'll have the chance to showcase your business to a targeted audience of exterior cleaning professionals and make a positive impact on the industry. Plus, you'll be supporting the development of a tool that brings transparency and efficiency to the quoting process, helping window cleaning companies save time and provide more accurate quotes to their customers. If you're interested in supporting the EzEval project and gaining valuable exposure for your business, don't hesitate to get in touch and learn more about sponsorship opportunities.</p>
+  </article>
+  <div class="alert alert-info shadow-lg">
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current flex-shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+      <span>Sponsor Application Form Coming Soon...</span>
+    </div>
+  </div>
+</div>
