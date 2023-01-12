dbe5c7140047f81842ae37b9339edc316b2f9fa4
vonroga <ryanroga@gmail.com>
2023-01-12 11:27:29 -0800

    Create '/login'

---
+++ app/src/routes/login/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import PageHeroSection from '$lib/components/PageHeroSection.svelte';
+  import H1 from '$lib/components/H1.svelte';
+</script>
+<PageHeroSection>
+  <H1>Login</H1>
+</PageHeroSection>
