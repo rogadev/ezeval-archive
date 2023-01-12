631e9697e0b2f00cdd707068c54dd8fb9d18eea2
vonroga <ryanroga@gmail.com>
2023-01-12 11:26:14 -0800

    Update NavbarLinks.svelte

--- app/src/lib/components/NavbarLinks.svelte
+++ app/src/lib/components/NavbarLinks.svelte
@@ -1,5 +1,12 @@
 <ul class="flex flex-row items-center gap-4">
-  <li><a href="/">Home</a></li>
-  <li><a href="/">About</a></li>
-  <li><a href="/">Contact</a></li>
+  <li><a class="hover:underline underline-offset-4" href="/">Home</a></li>
+  <li><a class="hover:underline underline-offset-4" href="/about">About</a></li>
+  <li><a href="/login" class="md:ml-3 bg-brand py-2 px-4 rounded text-black font-semibold">Login</a></li>
+  <li><a href="/register" class="bg-light py-2 px-4 rounded text-black font-semibold">Register</a></li>
 </ul>
+
+<style scoped>
+  a:hover {
+    opacity: 0.8;
+  }
+</style>
