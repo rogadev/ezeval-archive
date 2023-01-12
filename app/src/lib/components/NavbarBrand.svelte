a528e3485223e4cb3411b2efb0ff58b1a4142c96
vonroga <ryanroga@gmail.com>
2023-01-12 11:26:11 -0800

    Update NavbarBrand.svelte

--- app/src/lib/components/NavbarBrand.svelte
+++ app/src/lib/components/NavbarBrand.svelte
@@ -1,4 +1,4 @@
-<div class="grid grid-flow-col items-center align-middle gap-4 py-4">
-  <img src="https://via.placeholder.com/30" alt="C/O https://placeholder.com/" >
-  <span class="text-xl font-semibold hidden sm:block">Confusion Meter</span>
-</div>
+<a href="/" class="grid grid-flow-col items-center align-middle gap-4 py-4">
+  <img class="rounded" src="https://via.placeholder.com/40" alt="C/O https://placeholder.com/" >
+  <span class="text-xl font-semibold hidden sm:block">EzEval</span>
+</a>
