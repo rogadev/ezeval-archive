321ccb6a98b027dc443c74428c8feaaaa1036cc3
vonroga <ryanroga@gmail.com>
2023-01-17 15:06:15 -0800

    Fixed Issue with ENV Keys
    Now correctly importing dynamic ENV variables at runtime.

--- app/.svelte-kit/types/src/routes/$types.d.ts
+++ app/.svelte-kit/types/src/routes/$types.d.ts
@@ -8,9 +8,12 @@ export type RequiredKeys<T> = { [K in keyof T]-?: {} extends { [P in K]: T[K] } 
 type OutputDataShape<T> = MaybeWithVoid<Omit<App.PageData, RequiredKeys<T>> & Partial<Pick<App.PageData, keyof T & keyof App.PageData>> & Record<string, any>>
 type EnsureDefined<T> = T extends null | undefined ? {} : T;
 type OptionalUnion<U extends Record<string, any>, A extends keyof U = U extends U ? keyof U : never> = U extends unknown ? { [P in Exclude<A, keyof U>]?: never } & U : never;
-type LayoutRouteId = RouteId | null
+type PageParentData = EnsureDefined<LayoutData>;
+type LayoutRouteId = RouteId | "/" | "/about" | "/dashboard" | "/login" | "/register" | null
 type LayoutParams = RouteParams & {  }
 type LayoutParentData = EnsureDefined<{}>;
 
+export type PageServerData = null;
+export type PageData = Expand<PageParentData>;
 export type LayoutServerData = null;
 export type LayoutData = Expand<LayoutParentData>;
