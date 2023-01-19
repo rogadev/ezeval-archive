6baf578b27178a2bdf3ee688b02a67ac9f77ac6e
vonroga <ryanroga@gmail.com>
2023-01-19 15:01:14 -0800

    Auth Refactor.

--- app/src/hooks.server.ts
+++ app/src/hooks.server.ts
@@ -3,18 +3,25 @@ import { getServerSession } from '@supabase/auth-helpers-sveltekit';
 import type { Session } from '@supabase/supabase-js';
 import type { Handle, RequestEvent } from "@sveltejs/kit";
 import { fail, redirect } from "@sveltejs/kit";
+import inDevMode from "$lib/server/mode";
 
+/**
+ * Reaches out to our getServerSession function from @supabase/auth-helpers-sveltekit to get the current session and returns it, or null if there is no session.
+ * @param event Request event
+ * @returns Promise<Session | null>
+ */
 const getSupabaseSession = async (event: RequestEvent) => {
   let session: Session | null = null;
   try {
     session = await getServerSession(event);
   } catch (e) {
-    console.error('Supabase getServerSession Error: ', e);
+    if (inDevMode) console.error('Supabase getServerSession Error: ', e);
     fail(500, 'Something went wrong');
   }
   return session;
 };
 
+// This is the main function that will be called by SvelteKit to handle server requests.
 export const handle: Handle = async ({ event, resolve }) => {
   const session = await getSupabaseSession(event);
   const loggedIn = session?.user.email ? true : false;
@@ -23,7 +30,6 @@ export const handle: Handle = async ({ event, resolve }) => {
   const guardedPaths = ['/dashboard', '/profile', '/settings', '/admin'];
   const intendedPathIsGuarded = guardedPaths.some((guardedPath: string) => intendedPath.startsWith(guardedPath));
 
-  // If the user is not logged in and they are trying to access a guarded path, redirect them to the login page.
   if (!loggedIn && intendedPathIsGuarded) {
     throw redirect(303, '/login');
   }
--- app/src/routes/login/+page.server.ts
+++ app/src/routes/login/+page.server.ts
@@ -2,28 +2,14 @@ import { fail, redirect } from "@sveltejs/kit";
 import type { Actions } from "./$types";
 import type { Provider } from "@supabase/supabase-js";
 import { supabase } from "$lib/db";
-import type { PageServerLoad } from "./";
-import { getServerSession } from '@supabase/auth-helpers-sveltekit';
-
-export const load: PageServerLoad = async (event) => {
-  const session = await getServerSession(event);
-  if (session) {
-    console.log("Redirect to '/' from login page because we have a session.");
-    throw redirect(303, "/");
-  }
-  return {
-    session,
-  };
-};
+import devMode from "$lib/server/mode";
 
 export const actions: Actions = {
   login: async ({ url }) => {
-    const provider = url.searchParams.get("provider") as Provider;
-    console.log('before');
-    const { data, error: err } = await supabase.auth.signInWithOAuth({ provider }, { redirectTo: "http://localhost:3000/dashboard" });
-    console.log('after');
-    if (err) {
-      console.log(err);
+    const provider = url.searchParams.get("provider") satisfies Provider;
+    const { data, error } = await supabase.auth.signInWithOAuth({ provider }, { redirectTo: "http://localhost:3000/dashboard" });
+    if (error) {
+      if (devMode) console.error(`Error logging in with Supabase. Provider: ${provider}. Error: ${error}`);
       return fail(400, "Something went wrong");
     }
     throw redirect(303, data.url);
