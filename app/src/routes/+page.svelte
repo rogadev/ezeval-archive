add1fff831c030c0a0344e495222592053711a84
vonroga <ryanroga@gmail.com>
2023-01-19 10:20:26 -0800

    Update +page.svelte

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -13,25 +13,8 @@
 <PageHeroSection>
   <div class="grid grid-cols-2 items-center py-8">
     <div class="col-span-1">
-      <H1>EzEval</H1>
-      <p class="sm:text-[23px] md:text-xl font-semibold">Window Cleaning<br>Project Evaluation & Quotes</p>
-    </div>
-    <div class="col-span-1">
-      <div class="flex flex-row justify-end">
-        <div class="flex flex-row items-center">
-          <img src="https://via.placeholder.com/700x300.png?text=Graphic+Goes+Here" alt="C/O https://placeholder.com/">
-        </div>
-      </div>
+      <H1>Welcome</H1>
+      <p class="sm:text-[23px] md:text-xl font-semibold">Dashboard...</p>
     </div>
   </div>
 </PageHeroSection>
-
-<PageContentSection>
-  <div class="py-8">
-    <H2>Close More Deals</H2>
-    <p>Close more leads with a professional, easy to use, and mobile friendly project evaluation and quote tool.</p>
-    <div class="flex justify-center">
-      <Cta className="my-2"><a href="/register">Sign Up</a></Cta>
-    </div>
-  </div>
-</PageContentSection>
