3e094d0b74d6f19b5ecc8458a9d1699c36a03414
vonroga <ryanroga@gmail.com>
2023-01-19 11:27:18 -0800

    Update +page.server.ts
    Auth is NOT working as expected. Problem lies with hooks.server.ts and more information is needed.

--- app/src/routes/welcome/+page.server.ts
+++ app/src/routes/welcome/+page.server.ts
@@ -2,6 +2,14 @@ import { fail, redirect } from "@sveltejs/kit";
 import type { Actions } from "../login/$types";
 import type { Provider } from "@supabase/supabase-js";
 import { supabase } from "$lib/db";
+import type { PageServerLoad } from "./";
+import { getServerSession } from '@supabase/auth-helpers-sveltekit';
+
+export const load: PageServerLoad = async ({ locals }) => {
+  if (locals.user || locals.session) {
+    throw redirect(303, "/");
+  }
+};
 
 export const actions: Actions = {
   register: async ({ url }) => {
@@ -15,7 +23,7 @@ export const actions: Actions = {
   },
   login: async ({ url }) => {
     const provider = url.searchParams.get("provider") as Provider;
-    const { data, error: err } = await supabase.auth.signInWithOAuth({ provider });
+    const { data, error: err } = await supabase.auth.signInWithOAuth({ provider, redirectTo: "http://localhost:3000/" });
     if (err) {
       console.log(err);
       return fail(400, "Something went wrong");
